814eeb294fd4154c73caa989b34e796c6e8a0ef5
Bhushan Saundale <saundalebhushan5@gmail.com>
2020-04-09 11:26:29 +0530

    [BHUSHAN]Refactor:Extend Max Method with Taking More than Three parameters.;

--- src/main/java/com/bridgelabz/generics/MaximumNumber.java
+++ src/main/java/com/bridgelabz/generics/MaximumNumber.java
@@ -1,9 +1,12 @@
 package com.bridgelabz.generics;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class MaximumNumber<T extends Comparable> {
-        T number1;
-        T number2;
-        T number3;
+    T number1;
+    T number2;
+    T number3;
 
     public MaximumNumber(T number1, T number2, T number3) {
         this.number1=number1;
@@ -11,12 +14,7 @@ public class MaximumNumber<T extends Comparable> {
         this.number3=number3;
     }
 
-    public T getLargeNumber() {
-        return getLargeNumber(number1,number2,number3);
-
-    }
-
-    private <T extends Comparable> T getLargeNumber(T number1, T number2, T number3) {
+    public static <T extends Comparable> T getLargeNumber( T number1,T number2,T number3,T ...option) {
         T max=number1;
         if (number2.compareTo(max)>0){
             max = number2;
@@ -24,6 +22,16 @@ public class MaximumNumber<T extends Comparable> {
         if (number3.compareTo(max)>0) {
             max = number3;
         }
-        return max;
+        if(option.length !=0){
+            Arrays.sort(option, Collections.reverseOrder());
+            if (max.compareTo(option[0])<0){
+                max=option[0];
+            }
         }
+        return max;
+    }
+
+    public T getLargeNumber() {
+        return getLargeNumber(number1,number2,number3);
+    }
 }
--- src/test/java/com/bridgelabz/generics/MaximumNumberTest.java
+++ src/test/java/com/bridgelabz/generics/MaximumNumberTest.java
@@ -23,42 +23,54 @@ public class MaximumNumberTest {
     }
 
     @Test
-    public void givanFloatsMaxNumber_WhenAtThirdPosition_ShouldReturnFirstNumber() {
+    public void givenFloatsMaxNumber_WhenAtThirdPosition_ShouldReturnFirstNumber() {
         MaximumNumber maximumNumber = new MaximumNumber(10.5f, 15.5f, 20.5f);
         Assert.assertEquals(20.5f,maximumNumber.getLargeNumber());
     }
 
     @Test
-    public void givanFloatsMaxNumber_WhenAtSecondPosition_ShouldReturnFirstNumber() {
+    public void givenFloatsMaxNumber_WhenAtSecondPosition_ShouldReturnFirstNumber() {
         MaximumNumber maximumNumber = new MaximumNumber(10.0f, 88.5f, 12.4f);
         Assert.assertEquals(88.5f,maximumNumber.getLargeNumber());
 
     }
 
     @Test
-    public void givanFloatsMaxNumber_WhenAtFirstPosition_ShouldReturnFirstNumber() {
+    public void givenFloatsMaxNumber_WhenAtFirstPosition_ShouldReturnFirstNumber() {
         MaximumNumber maximumNumber = new MaximumNumber(44.3f, 22.3f, 11.3f);
         Assert.assertEquals(44.3f,maximumNumber.getLargeNumber());
     }
 
     @Test
-    public void givanStringMaxNumber_WhenAtThirdPosition_ShouldReturnFirstNumber() {
+    public void givenStringMax_WhenAtThirdPosition_ShouldReturnFirstString() {
         MaximumNumber maximumNumber = new MaximumNumber("Ajay","Shyam","Yogesh");
         Assert.assertEquals("Yogesh",maximumNumber.getLargeNumber());
     }
 
     @Test
-    public void givanStringMaxNumber_WhenAtSecondPosition_ShouldReturnFirstNumber() {
+    public void givenStringMax_WhenAtSecondPosition_ShouldReturnFirstString() {
         MaximumNumber maximumNumber = new MaximumNumber("Ram","Vishal","Chetan");
         Assert.assertEquals("Vishal",maximumNumber.getLargeNumber());
-
     }
 
     @Test
-    public void givanStringMaxNumber_WhenAtFirstPosition_ShouldReturnFirstNumber() {
+    public void givenStringMax_WhenAtFirstPosition_ShouldReturnFirstString() {
         MaximumNumber maximumNumber = new MaximumNumber("Tushar","Aman","Bhushan");
         Assert.assertEquals("Tushar",maximumNumber.getLargeNumber());
     }
 
+    @Test
+    public void givenMultipleIntegers_WhenIntegersIsMax_ShouldReturnMaxInteger() {
+        Assert.assertSame(8,MaximumNumber.getLargeNumber(6,5,3,7,8));
+    }
 
+    @Test
+    public void givenMultipleFloats_WhenFloatsIsMax_ShouldReturnMaxFloats() {
+        Assert.assertSame(5.9f,MaximumNumber.getLargeNumber(1.2f,2.1f,4.2f,5.9f,4.4f));
+    }
+
+    @Test
+    public void givenMultipleStrings_WhenStringIsMax_ShouldReturnMaxString() {
+        Assert.assertSame("Vijay",MaximumNumber.getLargeNumber("Baban","Prince","Dinesh","Sachin","Vijay"));
+    }
 }
